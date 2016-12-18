2ac5b42ac91aeee78856226863aa03f7c99bd974
Bryan Brito <bhyanbrito+github@gmail.com>
2016-12-18 19:51:31 -0300

    Fuction creation creat_deck()

---
+++ blackjack.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import random
+import pdb
+
+
+def creat_deck(deck=1):
+    '''
+    Create card function, it is possible to create a deck with more cards.
+    Increases the difficulty for the player. Maximum of 8 decks.
+    '''
+    suits = ["♣", "♦", "♥", "♠"]
+    numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q",
+                    "J", "K"]
+    decks = []
+    count = 0
+
+    while count < deck:
+        count += 1
+        for suit in suits:
+            for number in numbers:
+                decks.append('{}{}'.format(suit, number))
+
+    return decks
