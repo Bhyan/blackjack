c2026bff75dd4e33f57f03d9e90cae4e7c03d580
Bryan Brito <bhyanbrito+github@gmail.com>
2016-12-20 00:11:25 -0300

    Added docstring for bet function.

--- blackjack.py
+++ blackjack.py
@@ -20,14 +20,19 @@ def creat_deck(deck=1):
         for suit in suits:
             for number in numbers:
                 decks.append('{}{}'.format(suit, number))
-
     return decks
 
     def bet(coin, quantity):
+        '''
+        Betting values are defined as casino chips. That is why exist the
+        exception error.
+        '''
         global MONEY
-        if coin not in (1, 5, 10, 25, 50, 100, 500, 1000):
+        if coin not in (1, 5, 10, 25, 50, 100):
             raise Exception('Invalid coin for bet.')
-        elif MONEY >= (coin * quantity):
+        elif MONEY < (coin * quantity):
+            raise Exception('Value of bet larger what your money.')
+        else:
             MONEY -= (coin * quantity)
             return MONEY
 
