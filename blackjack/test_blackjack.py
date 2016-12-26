1ddfbec1e817bf75209421f7cf61cb6737c0c906
Bryan Brito <bhyanbrito+github@gmail.com>
2016-12-26 16:54:35 -0300

    Function test hit_house

--- blackjack/test_blackjack.py
+++ blackjack/test_blackjack.py
@@ -31,7 +31,7 @@ class TestBlackjack(unittest.TestCase):
     def test_bet(self):
         '''Function test of bet.'''
         # For a bet valid.
-        self.assertEqual(self.obj.money, 1000.01)
+        self.assertEqual(self.obj.money, 1000.00)
         # For a bet invalid.
         self.assertRaises(Exception, blackjack.Blackjack.bet, (3, 10))
         self.assertRaises(Exception, blackjack.Blackjack.bet, (100, 30))
@@ -74,6 +74,18 @@ class TestBlackjack(unittest.TestCase):
         fake_hand = ['J♣', 'A♠', '8♣']
         self.obj.show_points(fake_hand)
         self.assertRaises(Exception, blackjack.Blackjack.hit)
+
+    def test_hit_house(self):
+        '''
+        Function test of hit house.
+        '''
+        self.obj.show_points(self.fake_hand)
+        self.obj.hit_house()
+        self.assertEqual(len(self.obj.decks), 47)
+        # Exception.
+        fake_hand = ['J♣', 'A♠', '8♣']
+        self.obj.show_points(fake_hand)
+        self.assertRaises(Exception, blackjack.Blackjack.hit_house)
 
     def tearDown(self):
         pass
